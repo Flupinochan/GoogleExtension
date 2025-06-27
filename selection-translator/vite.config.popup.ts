c9eb235b6c860869e58df98bb80885935fc1f18f
Flupinochan <flupino@metalmental.net>
2025-06-27 12:43:57 +0900

    add popup and setting build

--- selection-translator/vite.config.popup.ts
+++ selection-translator/vite.config.popup.ts
@@ -7,33 +7,33 @@ const outputDir = "selection-translator";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-        // index.html を popup.html にリネームするための処理
+  plugins: [
+    react(),
+    // index.html を popup.html にリネームするための処理
     {
       name: "rename-html",
       closeBundle() {
         const oldPath = path.resolve(outputDir, "index.html");
-        const newPath = path.resolve(outputDir, "sidepanel.html");
+        const newPath = path.resolve(`${outputDir}/popup`, "popup.html");
         if (fs.existsSync(oldPath)) {
           fs.renameSync(oldPath, newPath);
-          console.log("index.html >> sidepanel.html にリネームしました");
+          console.log("index.html を popup.html にリネームしました");
         }
       }
     }
   ],
   build: {
-    lib: {
-      entry: 'src/popup/main.tsx',
-      name: 'popup',
-      fileName: 'popup',
-      formats: ['iife'] // 単一ファイル出力
-    },
-    rollupOptions: {
-      output: {
-        entryFileNames: 'popup/popup.js',
-      }
-    },
-    outDir: 'selection-translator',
+    outDir: outputDir,
     emptyOutDir: false,
+    rollupOptions: {
+      input: {
+        popup: path.resolve(__dirname, 'index.html')
+      },
+      output: {
+        entryFileNames: 'popup/[name].js',
+        chunkFileNames: 'popup/[name].js',
+        assetFileNames: 'popup/[name].[ext]'
+      }
+    }
   }
 })
