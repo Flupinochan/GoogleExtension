1c7e833722eb0c688943110d4332ccc58da63fad
Flupinochan <flupino@metalmental.net>
2025-06-28 19:17:46 +0900

    add icon

--- selection-translator/wxt.config.ts
+++ selection-translator/wxt.config.ts
@@ -11,6 +11,6 @@ export default defineConfig({
     description: "A tool that translates selected text and performs OCR on clicked images!",
     author: { email: "flupino@metalmental.net" },
     key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqsqQBqenHt9mmqSotfIo436FqN48TLUkrmLXU/x22xp44PR8z1qNmjAAqVj98oxsteXPbEcGXd8mOlSzKljtrFRiReXyo0MvfDn4UpDMQ6ZfqrMjccXTOOv7ygOPdCpBuFT2PmD+PiJWThzXOQcegvbliyNo1giH0BhtEyuWL7AN3iIW46YiLodrkTvG9EIXn81MjQa3HSLxf3MFTWPHMpxbCTAaG08utydcHOxzwqSvUK/uMtBWcWyHTttnTyxwdoQu2E1P7wK6/bvpNiniD/RAyoXtgjEShMs2IXTGoDU+E66fT+SNfilLR7gcvsOXiVhSNEFzRCOWZwJ8Eny/dQIDAQAB",
-    permissions: ['storage', 'contextMenus', 'scripting', 'activeTab'],
+    permissions: ['storage'],
   },
 });
